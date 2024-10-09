2e4a386eee78c989a88f88d3345ad0293c0e1198
Joshua Gonzales <codingjogo@gmail.com>
2024-10-09 15:26:00 +0800

    feat: Setup Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,48 @@
 import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Home() {
-  return (
-    <main className="container mx-auto py-8">
-      <Button>Hello</Button>
-    </main>
-  );
+	return (
+		<main className="container mx-auto py-8 flex items-center justify-center">
+			<form className="max-w-screen-sm">
+      <Card>
+				<CardHeader>
+					<CardTitle className="text-2xl font-semibold">Form Tutorial with Zod Validation</CardTitle>
+				</CardHeader>
+				<CardContent className="grid space-y-3">
+					<div className="flex items-center space-x-3">
+            {/* Name */}
+            <div>
+              <Label className="text-lead font-semibold">Name</Label>
+              <Input placeholder="Name"/> 
+            </div>
+            {/* Amount */}
+            <div>
+              <Label className="text-lead font-semibold">Amount</Label>
+              <Input placeholder="Amount"/> 
+            </div>
+          </div>
+
+          {/* Buttons for RFH */}
+          <div className="flex items-center space-x-3">
+            <Button type="button">Append</Button>
+            <Button type="button" variant={'secondary'}>Prepend</Button>
+          </div>
+				</CardContent>
+				<CardFooter>
+					<Button type="submit" className="w-full">Submit</Button>
+				</CardFooter>
+			</Card>
+
+      </form>
+		</main>
+	);
 }

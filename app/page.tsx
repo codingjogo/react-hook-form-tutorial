bf03fe8642940a4485debafc5588db62447a0e15
Joshua Gonzales <codingjogo@gmail.com>
2024-10-09 15:12:41 +0800

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="container mx-auto py-8">
+      <Button>Hello</Button>
+    </main>
+  );
+}
---
+++ constants.ts
@@ -0,0 +1,1 @@
+// CONSTANTS like email REGEX
